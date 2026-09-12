e0d68324c0b110bded177c304514cac2caf6f22c
ivanpv112 <ivan.padilla5116@alumnos.udg.mx>
2026-09-11 18:46:31 -0600

    Update index.php

--- profesor/index.php
+++ profesor/index.php
@@ -1,7 +1,9 @@
 <?php
 session_start();
 require '../db.php';
-require '../security.php';
+require_once '../security.php';
+
+validar_csrf_estricto('POST');
 
 // SEGURIDAD: Solo Profesores
 if (!isset($_SESSION['user_id']) || $_SESSION['rol'] !== 'PROFESOR') {
