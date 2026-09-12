cf62ed3465d86afc86aa1d343da0b9e20553e33f
ivanpv112 <ivan.padilla5116@alumnos.udg.mx>
2026-09-11 18:28:22 -0600

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
+require 'security.php';
+
+validar_csrf_estricto('GET');
+
 session_destroy(); // Destruye todos los datos de la sesión
 header("Location: index.php"); // Redirige al login
 exit;
