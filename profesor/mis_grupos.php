f05789396cec41e31aabaa3190afc1e2ae84e9e3
ivanpv112 <ivan.padilla5116@alumnos.udg.mx>
2026-09-11 18:46:04 -0600

    Update mis_grupos.php

--- profesor/mis_grupos.php
+++ profesor/mis_grupos.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
 require '../db.php';
+require_once '../security.php';
+
+validar_csrf_estricto('POST');
 
 // SEGURIDAD: Solo Profesores (Validación de Sesión y Rol)
 if (!isset($_SESSION['user_id']) || $_SESSION['rol'] !== 'PROFESOR') { 
