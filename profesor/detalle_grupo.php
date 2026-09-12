6eb985b97afd3a0f769c1b317840a3a5f6ceb99f
ivanpv112 <ivan.padilla5116@alumnos.udg.mx>
2026-09-11 18:47:25 -0600

    Update detalle_grupo.php

--- profesor/detalle_grupo.php
+++ profesor/detalle_grupo.php
@@ -1,7 +1,9 @@
 <?php
 session_start();
 require '../db.php';
-require '../security.php';
+require_once '../security.php';
+
+validar_csrf_estricto('POST');
 
 if (!isset($_SESSION['user_id']) || $_SESSION['rol'] !== 'PROFESOR') { header("Location: ../index.php"); exit; }
 
